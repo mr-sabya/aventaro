aa76bcd774d49137d335aa99c0d15f66bdd6704b
Sabya Roy <68545445+mr-sabya@users.noreply.github.com>
2025-11-10 00:19:29 +0600

    refactor: replace tour and discover sections with Livewire components

---
+++ resources/views/livewire/frontend/components/discover-section.blade.php
@@ -0,0 +1,221 @@
+<section class="tour-descover-section section-padding fix bg-cover"
+    style="background-image: url(assets/frontend/img/tour/bg2.jpg);">
+    <div class="container">
+        <div class="tour-discover-wrapper">
+            <div class="row g-4">
+                <div class="col-xl-5">
+                    <div class="tour-content lg-center">
+                        <div class="section-title">
+                            <span class="wow fadeInUp">Discover Weekly Travelling</span>
+                            <h2 class="wow fadeInUp" data-wow-delay=".3s">Hot deals on <br> select expedition
+                                departures</h2>
+                            <p class="mt-3 mt-mb-0 wow fadeInUp" data-wow-delay=".5s">
+                                Curated destinations and tours that capture the true <br> essence of location,
+                                ensuring you experience. Our <br> attraction pass save you more.
+                            </p>
+                        </div>
+                        <div class="tour-button mt-3">
+                            <a href="tour.html" class="theme-btn wow fadeInUp" data-wow-delay=".5s">
+                                <span>Explore More</span> <i class="far fa-long-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-7">
+                    <div class="swiper tour-slider">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="tour-card-item">
+                                    <div class="tour-image">
+                                        <img src="{{ url('assets/frontend/img/tour/29.jpg') }}" alt="img">
+                                    </div>
+                                    <div class="tour-content">
+                                        <h6>From <span>$270</span> $399</h6>
+                                        <h4>
+                                            <a href="tour-details.html">
+                                                Marvel at Majestic Mountains and <br> Lakes in Banff, Canada
+                                            </a>
+                                        </h4>
+                                        <ul>
+                                            <li>
+                                                <i class="far fa-map-marker-alt"></i>
+                                                132, Tic St, Wingston, New York 12401
+                                            </li>
+                                        </ul>
+                                        <div class="list">
+                                            <ul>
+                                                <li>
+                                                    <i class="far fa-calendar"></i>
+                                                    4 Days / 5 Night
+                                                </li>
+                                            </ul>
+                                            <div class="star">
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="far fa-star"></i></a>
+                                                <span>(30)</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="tour-card-item">
+                                    <div class="tour-image">
+                                        <img src="{{ url('assets/frontend/img/tour/30.jpg') }}" alt="img">
+                                    </div>
+                                    <div class="tour-content">
+                                        <h6>From <span>$270</span> $399</h6>
+                                        <h4>
+                                            <a href="tour-details.html">
+                                                Step Back in Time at Ancient Ruins <br> in Rome, Italy
+                                            </a>
+                                        </h4>
+                                        <ul>
+                                            <li>
+                                                <i class="far fa-map-marker-alt"></i>
+                                                132, Tic St, Wingston, New York 12401
+                                            </li>
+                                        </ul>
+                                        <div class="list">
+                                            <ul>
+                                                <li>
+                                                    <i class="far fa-calendar"></i>
+                                                    4 Days / 5 Night
+                                                </li>
+                                            </ul>
+                                            <div class="star">
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="far fa-star"></i></a>
+                                                <span>(30)</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="tour-card-item">
+                                    <div class="tour-image">
+                                        <img src="{{ url('assets/frontend/img/tour/31.jpg') }}" alt="img">
+                                    </div>
+                                    <div class="tour-content">
+                                        <h6>From <span>$270</span> $399</h6>
+                                        <h4>
+                                            <a href="tour-details.html">
+                                                Uncover Vibrant Culture and History in Lisbon, Portugal
+                                            </a>
+                                        </h4>
+                                        <ul>
+                                            <li>
+                                                <i class="far fa-map-marker-alt"></i>
+                                                132, Tic St, Wingston, New York 12401
+                                            </li>
+                                        </ul>
+                                        <div class="list">
+                                            <ul>
+                                                <li>
+                                                    <i class="far fa-calendar"></i>
+                                                    4 Days / 5 Night
+                                                </li>
+                                            </ul>
+                                            <div class="star">
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="far fa-star"></i></a>
+                                                <span>(30)</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="tour-card-item">
+                                    <div class="tour-image">
+                                        <img src="{{ url('assets/frontend/img/tour/30.jpg') }}" alt="img">
+                                    </div>
+                                    <div class="tour-content">
+                                        <h6>From <span>$270</span> $399</h6>
+                                        <h4>
+                                            <a href="tour-details.html">
+                                                Step Back in Time at Ancient Ruins <br> in Rome, Italy
+                                            </a>
+                                        </h4>
+                                        <ul>
+                                            <li>
+                                                <i class="far fa-map-marker-alt"></i>
+                                                132, Tic St, Wingston, New York 12401
+                                            </li>
+                                        </ul>
+                                        <div class="list">
+                                            <ul>
+                                                <li>
+                                                    <i class="far fa-calendar"></i>
+                                                    4 Days / 5 Night
+                                                </li>
+                                            </ul>
+                                            <div class="star">
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="far fa-star"></i></a>
+                                                <span>(30)</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="tour-card-item">
+                                    <div class="tour-image">
+                                        <img src="{{ url('assets/frontend/img/tour/31.jpg') }}" alt="img">
+                                    </div>
+                                    <div class="tour-content">
+                                        <h6>From <span>$270</span> $399</h6>
+                                        <h4>
+                                            <a href="tour-details.html">
+                                                Uncover Vibrant Culture and History in Lisbon, Portugal
+                                            </a>
+                                        </h4>
+                                        <ul>
+                                            <li>
+                                                <i class="far fa-map-marker-alt"></i>
+                                                132, Tic St, Wingston, New York 12401
+                                            </li>
+                                        </ul>
+                                        <div class="list">
+                                            <ul>
+                                                <li>
+                                                    <i class="far fa-calendar"></i>
+                                                    4 Days / 5 Night
+                                                </li>
+                                            </ul>
+                                            <div class="star">
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="fas fa-star"></i></a>
+                                                <a href="#"><i class="far fa-star"></i></a>
+                                                <span>(30)</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-dot4 mt-5">
+                        <div class="dot"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
